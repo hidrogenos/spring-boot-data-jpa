950c1710a5ddf804485f4473991a57091c1a1cf7
Andres Ruge <hidrogenos00@gmail.com>
2018-10-31 00:29:49 -0500

    clase 98
    clase 98

--- src/main/java/com/bolsadeideas/springboot/app/model/service/ClienteServiceImpl.java
+++ src/main/java/com/bolsadeideas/springboot/app/model/service/ClienteServiceImpl.java
@@ -45,7 +45,7 @@ public class ClienteServiceImpl implements IClienteService {
 	@Transactional(readOnly = true)
 	public Cliente findOne(Long id) {
 		// TODO Auto-generated method stub
-		return clienteDao.findOne(id);
+		return clienteDao.findById(id).orElse(null);
 	}
 	
 	@Override
@@ -58,7 +58,7 @@ public class ClienteServiceImpl implements IClienteService {
 	@Transactional
 	public void delete(Long id) {
 		// TODO Auto-generated method stub
-		clienteDao.delete(id);
+		clienteDao.deleteById(id);;
 		
 	}
 
@@ -85,19 +85,19 @@ public class ClienteServiceImpl implements IClienteService {
 	@Override
 	@Transactional(readOnly = true)
 	public Producto findProductoById(Long id) {
-		return productoDao.findOne(id);
+		return productoDao.findById(id).orElse(null);
 	}
 
 	@Override
 	@Transactional(readOnly = true)
 	public Factura findFacturaById(Long id) {
-		return facturaDao.findOne(id);
+		return facturaDao.findById(id).orElse(null);
 	}
 
 	@Override
 	@Transactional
 	public void deleteFactura(Long id) {
-		facturaDao.delete(id);
+		facturaDao.deleteById(id);
 	}
 
 	@Override
